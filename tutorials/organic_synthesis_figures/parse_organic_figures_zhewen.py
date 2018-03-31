5ae7c0c8d995ae75872f894580446d780c0042f6
hexy92 <hexy717@163.com>
2018-03-30 22:35:20 -0500

    nothing

--- tutorials/organic_synthesis_figures/parse_organic_figures_zhewen.py
+++ tutorials/organic_synthesis_figures/parse_organic_figures_zhewen.py
@@ -21,17 +21,17 @@ Org_Fig = candidate_subclass('Org_Fig', ['product','figure'])
 
 from fonduer import HTMLPreprocessor, OmniParser
 
-docs_path = os.environ['FONDUERHOME'] + 'tutorials/organic_synthesis_figures/testdata/html/'
-pdf_path = os.environ['FONDUERHOME'] + 'tutorials/organic_synthesis_figures/testdata/pdf/'
+docs_path = os.environ['FONDUERHOME'] + 'tutorials/organic_synthesis_figures/data/html/'
+pdf_path = os.environ['FONDUERHOME'] + 'tutorials/organic_synthesis_figures/data/pdf/'
 
-max_docs = float(1)
+max_docs = float(10)
 doc_preprocessor = HTMLPreprocessor(docs_path, max_docs=max_docs)
 corpus_parser = OmniParser(structural=True, lingual=True, visual=True, pdf_path=pdf_path,
 #                           flatten=['sup', 'sub', 'small'],
 #                           ignore=['italic', 'bold'],
                            blacklist=['style', 'script', 'meta', 'noscript'])
 
-corpus_parser.apply(doc_preprocessor, parallelism=PARALLEL)
+# corpus_parser.apply(doc_preprocessor, parallelism=PARALLEL)
 
 from fonduer import Document
 
@@ -65,7 +65,7 @@ dashes_rgx = '(\w*(\-?\d+\'?,\d+\'?\-?|\-[a-z]+\-)\w*)'
 ions_rgx = '([A-Z]+[a-z]*\d*\+)'
 #abbr_rgx = '([A-Z|\-][A-Z|\-]+)'
 prod_matcher = RegexMatchSplitEach(rgx='|'.join([prefix_rgx, suffix_rgx, ions_rgx]),
-                              longest_match_only=True, ignore_case=False)
+                              longest_match_only=False, ignore_case=False)
 
 
 from fonduer import CandidateExtractor
@@ -82,14 +82,42 @@ prod_ngrams = OmniNgramsProd(parts_by_doc=None, n_max=3)
 
 from fonduer.matchers import LambdaFunctionFigureMatcher
 
-def do_nothing_matcher(fig):
-    w_list = ['synthesis', ]
-    b_list = ['spectra', 'X-ray', 'Copyright']
+def white_black_list_matcher(fig):
+    white_list = ['synthesis', 'plausible']
+    black_list = ['spectra', 'x-ray', 'copyright']
 
-
+    fig_desc_wlist = fig.figure.description.split(" ")
+    fig_desc_wlist = [word.lower() for word in fig_desc_wlist]
+    # ocr_wlist = fig.figure.text.split("\n")
+    w = b = False
+    if any(v for v in fig_desc_wlist if v in white_list): w = True
+    if any(v for v in fig_desc_wlist if v in black_list): b = True
+    if b and (not w):
+        return False
     return True
 
-fig_matcher = LambdaFunctionFigureMatcher(func=do_nothing_matcher)
+def contain_organic_matcher(fig):
+
+
+    fig_desc_wlist = fig.figure.description.split(" ")
+    fig_desc_wlist = [word.lower() for word in fig_desc_wlist]
+    ocr_wlist = fig.figure.text.split("\n")
+    if any(re.search(prod_matcher.pattern, v) for v in fig_desc_wlist): return True
+    for word in ocr_wlist:
+        if not word or len(word.strip()) == 0:
+            continue
+        wl = word.strip().split(" ")
+        wl = [w.lower() for w in wl]
+        for v in wl:
+            if prod_matcher.f(v):
+                return True
+    return False
+
+
+fig_matcher1 = LambdaFunctionFigureMatcher(func=white_black_list_matcher)
+fig_matcher2 = LambdaFunctionFigureMatcher(func=white_black_list_matcher)
+fig_matcher = Union(fig_matcher1, fig_matcher2)
+
 from fonduer.candidates import OmniDetailedFigures
 
 figs = OmniDetailedFigures()
