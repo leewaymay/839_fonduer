ca3d9bd18bd8ec7229380b2b921bbb0c34e9f837
hexy717@163.com <hexy717@163.com>
2018-04-30 17:23:27 -0500

    add some gold data

--- tutorials/organic_synthesis_figures/organic_lfs.py
+++ tutorials/organic_synthesis_figures/organic_lfs.py
@@ -13,12 +13,12 @@ def LF_fig_name_match(c):
     else:
         return 0
 
-def LF_match_page(c):
-    args = c.get_contexts()
-    if len(args) != 2:
-        raise NotImplementedError("Only handles binary candidates currently")
-    organic, figure, = args
-    return 1 if is_same_org_fig_page(organic, figure) else -1
+# def LF_match_page(c):
+#     args = c.get_contexts()
+#     if len(args) != 2:
+#         raise NotImplementedError("Only handles binary candidates currently")
+#     organic, figure, = args
+#     return -1 if not is_same_org_fig_page(organic, figure) else 0
 
 
 def LF_text_desc_match(c):
@@ -28,7 +28,7 @@ def LF_text_desc_match(c):
     product, img = args
 
     # TODO: increase ratio?
-    if fuzz.partial_ratio(product.text, img.description) >= 70:
+    if fuzz.partial_ratio(product.text, img.description) >= 85:
         return 1
     elif fuzz.partial_ratio(product.text, img.description) <= 40:
         return -1
@@ -44,7 +44,7 @@ def LF_ocr_text_match(c):
     ocr_wordlist = figure.text.lower().split('\n')
     ocr_wordlist = [w for w in ocr_wordlist if not w == '']
     for w in ocr_wordlist:
-        if fuzz.partial_ratio(organic.text, w) >= 90:
+        if fuzz.partial_ratio(organic.text, w) >= 92:
             return 1
     return -1
 
@@ -54,18 +54,18 @@ def LF_text_length_match(c):
     if len(args) != 2:
         raise NotImplementedError("Only handles binary candidates currently")
     organic, figure, = args
-    return -1 if len(organic.text) < 5 else 0
+    return -1 if len(organic.text) < 6 else 0
 
 
-def LF_match_whitelist(c):
-    args = c.get_contexts()
-    if len(args) != 2:
-        raise NotImplementedError("Only handles binary candidates currently")
-    organic, figure, = args
-    whitelist = ['synthesis', 'syntheses', 'made', 'catalyze', 'generate', 'product', 'produce',
-            'formation', 'developed', 'approach', 'yields', 'reaction', 'mechanism', 'proposed',
-            'fig', 'scheme', 'graph', 'diagram', 'table']
-    return 1 if both_contain_keywords(organic, figure, whitelist) else 0
+# def LF_match_whitelist(c):
+#     args = c.get_contexts()
+#     if len(args) != 2:
+#         raise NotImplementedError("Only handles binary candidates currently")
+#     organic, figure, = args
+#     whitelist = ['synthesis', 'syntheses', 'made', 'catalyze', 'generate', 'product', 'produce',
+#             'formation', 'developed', 'approach', 'yields', 'reaction', 'mechanism', 'proposed',
+#             'fig', 'scheme', 'graph', 'diagram', 'table']
+#     return 1 if both_contain_keywords(organic, figure, whitelist) else 0
 
 def LF_match_blacklist(c):
     args = c.get_contexts()
@@ -76,19 +76,19 @@ def LF_match_blacklist(c):
     return -1 if organic.text in blacklist else 0
 
 
-def LF_pos_near(c):
-    args = c.get_contexts()
-    if len(args) != 2:
-        raise NotImplementedError("Only handles binary candidates currently")
-    organic, figure, = args
-    return 1 if org_pos_near_fig(organic, figure) else 0
+# def LF_pos_near(c):
+#     args = c.get_contexts()
+#     if len(args) != 2:
+#         raise NotImplementedError("Only handles binary candidates currently")
+#     organic, figure, = args
+#     return 1 if org_pos_near_fig(organic, figure) else 0
 
-def LF_organic_compound(c):
-    args = c.get_contexts()
-    if len(args) != 2:
-        raise NotImplementedError("Only handles binary candidates currently")
-    organic, figure, = args
-    return 1 if all([re.search(org_rgx, w) is not None for w in organic.text.split(' ')]) else 0
+# def LF_organic_compound(c):
+#     args = c.get_contexts()
+#     if len(args) != 2:
+#         raise NotImplementedError("Only handles binary candidates currently")
+#     organic, figure, = args
+#     return 1 if all([re.search(org_rgx, w) is not None for w in organic.text.split(' ')]) else 0
 
 def LF_synthesis_of(c):
     args = c.get_contexts()
@@ -116,27 +116,38 @@ def LF_product_of(c):
         return 1 if pro_idx + 2 == org_idx else -1
     return 0
 
-def LF_first_period(c):
+# def LF_first_period(c):
+#     args = c.get_contexts()
+#     if len(args) != 2:
+#         raise NotImplementedError("Only handles binary candidates currently")
+#     organic, figure, = args
+#     if '.' in figure.description:
+#         first = figure.description.split('.')[0]
+#         return 1 if organic.text in first else 0
+#     return 0
+
+def LF_page_not_match(c):
     args = c.get_contexts()
     if len(args) != 2:
         raise NotImplementedError("Only handles binary candidates currently")
     organic, figure, = args
-    if '.' in figure.description:
-        first = figure.description.split('.')[0]
-        return 1 if organic.text in first else 0
-    return 0
+    if abs(max(organic.sentence.page) - figure.page) > 1 or abs(min(organic.sentence.page) - figure.page) > 1:
+        return -1
+    else:
+        return 0
+
 
 org_fig_lfs = [
     LF_fig_name_match,
     LF_text_desc_match,
     LF_ocr_text_match,
     LF_text_length_match,
-    LF_match_whitelist,
+    # LF_match_whitelist,
     LF_match_blacklist,
-    LF_match_page,
-    LF_pos_near,
-    LF_organic_compound,
+    LF_page_not_match,
+    # LF_pos_near,
+    # LF_organic_compound,
     LF_synthesis_of,
     LF_product_of,
-    LF_first_period,
+    # LF_first_period,
 ]
