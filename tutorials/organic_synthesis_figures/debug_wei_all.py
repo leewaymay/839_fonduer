901114a0f0ee9bad077f6ebb711e87a6981a335f
leewaymay <leewaymay@gmail.com>
2018-04-29 21:29:35 -0500

    update main run python

--- tutorials/organic_synthesis_figures/debug_wei_all.py
+++ tutorials/organic_synthesis_figures/debug_wei_all.py
@@ -1,124 +1,53 @@
-import os
-import sys
+from fonduer import SnorkelSession, candidate_subclass, Document
+from fonduer import candidate_subclass
+from .set_env import set_up
+from scipy import sparse
+from fonduer import HTMLPreprocessor, OmniParser
+from fonduer.candidates import OmniDetailedFigures
+from .product_spaces import OmniNgramsProd
+from fonduer import CandidateExtractor
+from fonduer import BatchLabelAnnotator
+from .organic_lfs import *
+
 
 PARALLEL = 1 # assuming a quad-core machine
-ATTRIBUTE = "organic_figure"
-
-os.environ['FONDUERHOME'] = '/Users/liwei/BoxSync/s2016/Dropbox/839_fonduer'
-os.environ['FONDUERDBNAME'] = ATTRIBUTE
-os.environ['SNORKELDB'] = 'postgres://localhost:5432/' + os.environ['FONDUERDBNAME']
-
-
-from fonduer import SnorkelSession
-
 session = SnorkelSession()
-
-from fonduer import candidate_subclass
-
 Org_Fig = candidate_subclass('Org_Fig', ['organic','figure'])
+docs_path, pdf_path = set_up(name='wei')
 
-from fonduer import HTMLPreprocessor, OmniParser
+restart = True
+with_image_features = True
+max_docs = 24
+splits = 5 / 6
 
-docs_path = os.environ['FONDUERHOME'] + '/tutorials/organic_synthesis_figures/data/html/'
-pdf_path = os.environ['FONDUERHOME'] + '/tutorials/organic_synthesis_figures/data/pdf/'
+doc_preprocessor = HTMLPreprocessor(docs_path, max_docs=max_docs)
+corpus_parser = OmniParser(structural=True, lingual=True, visual=True, pdf_path=pdf_path,
+                           blacklist=['style', 'script', 'meta', 'noscript'])
 
-from fonduer import Document, Phrase, Figure
+if restart:
+    corpus_parser.apply(doc_preprocessor, parallelism=PARALLEL)
 
 docs = session.query(Document).order_by(Document.name).all()
 ld   = len(docs)
 
 train_docs = set()
-dev_docs   = set()
 test_docs  = set()
-splits = (1, 1)
 data = [(doc.name, doc) for doc in docs]
 data.sort(key=lambda x: x[0])
 for i, (doc_name, doc) in enumerate(data):
-    if i < splits[0] * ld:
+    if i < splits * ld:
         train_docs.add(doc)
-    elif i < splits[1] * ld:
-        dev_docs.add(doc)
     else:
         test_docs.add(doc)
 from pprint import pprint
 pprint([x.name for x in train_docs])
 
-from fonduer.snorkel.matchers import RegexMatchSpan, RegexMatchSplitEach,\
-    DictionaryMatch, LambdaFunctionMatcher, Intersect, Union
-
-prefix_rgx = '(\(?((mono|bi|di|tri|tetra|hex|hept|oct|iso|a?cycl|poly).+)?(meth|carb|benz|fluoro|chloro|bromo|iodo|hydroxy|amino|alk).+)'
-suffix_rgx = '(.+(ane|yl|adiene|atriene|yne|anol|anediol|anetriol|anone|acid|amine|xide|dine|(or?mone)|thiol)\)?)'
-
-dash_rgx = '((\w+\-|\(?)([a-z|\d]\'?\-)\w*)'
-comma_dash_rgx = '((\w+\-|\(?)([a-z|\d]\'?,[a-z|\d]\'?\-)\w*)'
-inorganic_rgx = '(([A-Z][a-z]?\d*\+?){2,})'
-
-org_rgx = '|'.join([prefix_rgx, suffix_rgx, dash_rgx, comma_dash_rgx, inorganic_rgx])
-
-rgx_matcher = RegexMatchSplitEach(rgx=org_rgx, longest_match_only=True, ignore_case=False)
-
-blacklist = ['CAS', 'PDF', 'RSC', 'SAR', 'TEM']
-prod_blacklist_lambda_matcher = LambdaFunctionMatcher(func=lambda x: x.text not in blacklist, ignore_case=False)
-blacklist_rgx = ['methods?.?']
-prod_blacklist_rgx_lambda_matcher = LambdaFunctionMatcher(
-    func=lambda x: all([re.match(r, x.text) is None for r in blacklist_rgx]), ignore_case=False)
-
-#prod_matcher = rgx_matcher
-prod_matcher = Intersect(rgx_matcher, prod_blacklist_lambda_matcher, prod_blacklist_rgx_lambda_matcher)
-
-from fonduer import CandidateExtractor
-from fonduer.lf_helpers import *
-import re
-
-def candidate_filter(c):
-    (organic, figure) = c
-    if same_file(organic, figure):
-        if mentionsFig(organic, figure) or mentionsOrg(figure, organic):
-            return True
-
-
-from product_spaces import OmniNgramsProd
 prod_ngrams = OmniNgramsProd(parts_by_doc=None, n_max=3)
-
-from fonduer.matchers import LambdaFunctionFigureMatcher
-
-def white_black_list_matcher(fig):
-    # print("enter filter 1")
-    # enter_time = time.time()
-    white_list = ['synthesis', 'plausible']
-    black_list = ['spectra', 'x-ray', 'copyright', 'structur', 'application']
-
-    fig_desc = fig.figure.description.lower()
-    in_white = in_black = False
-    if any(fig_desc.find(v) >= 0 for v in white_list): in_white = True
-    if any(fig_desc.find(v) >= 0 for v in black_list): in_black = True
-    if in_black and (not in_white):
-        return False
-    # print("{} has passed filter 1 in {} seconds!".format(fig.figure.name, time.time()-enter_time))
-    return True
-
-def contain_organic_matcher(fig):
-    # print("{} has failed filter 1 in {} seconds!".format(fig.figure.name, time.time() - enter_time))
-    # filter 2
-    desc_wordlist = fig.figure.description.lower().split(' ')
-    if any(re.search(org_rgx, w) for w in desc_wordlist): return True
-    if not fig.figure.text == '':
-        orc_wordlist = fig.figure.text.lower().split('\n')
-        orc_wordlist = [w for w in orc_wordlist if not w == '']
-        if any(re.search(org_rgx, w) for w in orc_wordlist): return True
-
-    print(fig.figure.name + " " + fig.figure.description)
-    return False
-
-fig_matcher1 = LambdaFunctionFigureMatcher(func=white_black_list_matcher)
-fig_matcher2 = LambdaFunctionFigureMatcher(func=contain_organic_matcher)
-fig_matcher = Union(fig_matcher1, fig_matcher2)
-# fig_matcher = LambdaFunctionFigureMatcher(func=figure_filter)
-
-
-from fonduer.candidates import OmniDetailedFigures
+fig_matcher = get_fig_matcher()
 
 figs = OmniDetailedFigures()
+org_rgx = get_rgx_matcher()
+prod_matcher = get_organic_matcher()
 
 candidate_extractor = CandidateExtractor(Org_Fig,
                         [prod_ngrams, figs],
@@ -126,6 +55,63 @@ candidate_extractor = CandidateExtractor(Org_Fig,
                         candidate_filter=candidate_filter)
 
 candidate_extractor.apply(train_docs, split=0, parallelism=PARALLEL)
+candidate_extractor.apply(test_docs, split=1, parallelism=PARALLEL)
 
 train_cands = session.query(Org_Fig).filter(Org_Fig.split == 0).all()
-print("Number of candidates:", len(train_cands))
+test_cands = session.query(Org_Fig).filter(Org_Fig.split == 1).all()
+print("Number of train candidates: {}\nNumber of test candidates: {}".format(len(train_cands), len(test_cands)))
+
+from fonduer import BatchFeatureAnnotator
+from fonduer.features.features import get_organic_image_feats
+from fonduer.features.read_images import gen_image_features
+
+# Only need to do this once
+print('Generating image features')
+if restart:
+    gen_image_features(docs_path=docs_path)
+
+featurizer = BatchFeatureAnnotator(Org_Fig, f=get_organic_image_feats)
+print('Generating other features')
+F_train = featurizer.apply(split=0, replace_key_set=True, parallelism=PARALLEL) # generate sparse features
+F_test = featurizer.apply(split=1, replace_key_set=True, parallelism=PARALLEL) # generate sparse features
+print('Merging image features')
+
+
+if with_image_features:
+    F_train = sparse.hstack(featurizer.load_matrix_and_image_features(split=0))  # concatenate dense with sparse matrix
+    F_test = sparse.hstack(featurizer.load_matrix_and_image_features(split=1))  # concatenate dense with sparse matrix
+else:
+    F_train = featurizer.load_matrix(split=0)
+    F_test = featurizer.load_matrix(split=1)
+
+print(F_train.shape)
+print(F_test.shape)
+
+labeler = BatchLabelAnnotator(Org_Fig, lfs=org_fig_lfs)
+
+L_train = labeler.apply(split=0, clear=True, parallelism=PARALLEL)
+
+print(L_train.shape)
+
+L_train.get_candidate(session, 0)
+
+from fonduer import GenerativeModel
+
+gen_model = GenerativeModel()
+gen_model.train(L_train, epochs=500, decay=0.9, step_size=0.001/L_train.shape[0], reg_param=0)
+train_marginals = gen_model.marginals(L_train)
+
+from fonduer import SparseLogisticRegression
+
+disc_model = SparseLogisticRegression()
+disc_model.train(F_train, train_marginals, n_epochs=200, lr=0.001)
+
+test_candidates = [F_test.get_candidate(session, i) for i in range(F_test.shape[0])]
+test_score = disc_model.predictions(F_test)
+true_pred = [test_candidates[_] for _ in np.nditer(np.where(test_score > 0))]
+
+train_score = disc_model.predictions(F_train)
+
+for i, cand in enumerate(train_cands):
+    print(cand.organic.text, '||||', cand.figure.url, train_score[i])
+
--- tutorials/organic_synthesis_figures/regex_matcher.py
+++ tutorials/organic_synthesis_figures/regex_matcher.py
@@ -1,3 +1,7 @@
+from fonduer.snorkel.matchers import LambdaFunctionMatcher, Intersect, Union
+from fonduer.snorkel.matchers import RegexMatchSpan
+import re
+from fonduer.lf_helpers import *
 
 def get_rgx_matcher():
 
@@ -11,3 +15,58 @@ def get_rgx_matcher():
     org_rgx = '|'.join([prefix_rgx, suffix_rgx, dash_rgx, comma_dash_rgx, inorganic_rgx])
 
     return org_rgx
+
+def get_organic_matcher():
+    org_rgx = get_rgx_matcher()
+
+    rgx_matcher = RegexMatchSpan(rgx=org_rgx, longest_match_only=True, ignore_case=False)
+    blacklist = ['CAS', 'PDF', 'RSC', 'SAR', 'TEM']
+    prod_blacklist_lambda_matcher = LambdaFunctionMatcher(func=lambda x: x.text not in blacklist, ignore_case=False)
+    blacklist_rgx = ['methods?.?']
+    prod_blacklist_rgx_lambda_matcher = LambdaFunctionMatcher(
+        func=lambda x: all([re.match(r, x.text) is None for r in blacklist_rgx]), ignore_case=False)
+
+    # prod_matcher = rgx_matcher
+    return Intersect(rgx_matcher, prod_blacklist_lambda_matcher, prod_blacklist_rgx_lambda_matcher)
+
+from fonduer.matchers import LambdaFunctionFigureMatcher
+
+def white_black_list_matcher(fig):
+    # print("enter filter 1")
+    # enter_time = time.time()
+    white_list = ['synthesis', 'plausible']
+    black_list = ['spectra', 'x-ray', 'copyright', 'structur', 'application']
+
+    fig_desc = fig.figure.description.lower()
+    in_white = in_black = False
+    if any(fig_desc.find(v) >= 0 for v in white_list): in_white = True
+    if any(fig_desc.find(v) >= 0 for v in black_list): in_black = True
+    if in_black and (not in_white):
+        return False
+    # print("{} has passed filter 1 in {} seconds!".format(fig.figure.name, time.time()-enter_time))
+    return True
+
+def contain_organic_matcher(fig):
+    # print("{} has failed filter 1 in {} seconds!".format(fig.figure.name, time.time() - enter_time))
+    # filter 2
+    desc_wordlist = fig.figure.description.lower().split(' ')
+    if any(re.search(org_rgx, w) for w in desc_wordlist): return True
+    if not fig.figure.text == '':
+        orc_wordlist = fig.figure.text.lower().split('\n')
+        orc_wordlist = [w for w in orc_wordlist if not w == '']
+        if any(re.search(org_rgx, w) for w in orc_wordlist): return True
+
+    #print(fig.figure.name + " " + fig.figure.description)
+    return False
+
+def get_fig_matcher():
+    fig_matcher1 = LambdaFunctionFigureMatcher(func=white_black_list_matcher)
+    fig_matcher2 = LambdaFunctionFigureMatcher(func=contain_organic_matcher)
+    fig_matcher = Union(fig_matcher1, fig_matcher2)
+    return fig_matcher
+
+def candidate_filter(c):
+    (organic, figure) = c
+    if same_file(organic, figure):
+        if mentionsFig(organic, figure) or mentionsOrg(figure, organic):
+            return True
